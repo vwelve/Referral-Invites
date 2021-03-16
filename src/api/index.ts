255428228d32f2eb7a019652ab58650f622e627d
Dominic Humber <dhumber2017@gmail.com>
2021-03-15 20:47:32 -0400

    Change a few things with folders and downloads

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,8 @@
 import express from 'express';
 import router from './routes/index';
 import ExpressSession from 'express-session';
-import dotenv from 'dotenv';
 import config from './utils/config';
 import { v4 } from 'uuid';
-
-dotenv.config({ path: __dirname + '/.env' });
 
 const app = express();
 
--- src/api/utils/config.ts
+++ src/api/utils/config.ts
@@ -1,11 +1,22 @@
 import { v4 } from "uuid";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default {
     db: {
         host: process.env.DB_HOST || "localhost",
         user: process.env.DB_USER || "root",
-        password: process.env.DB_PASSWORD,
+        password: process.env.DB_PASSWORD!,
         database: process.env.DB_NAME || "referral_invites"
+    },
+    discord: {
+        clientId: process.env.CLIENT_ID!,
+        clientSecret: process.env.CLIENT_SECRET!,
+        guildId: process.env.GUILD_ID!,
+        redirectUri: process.env.REDIRECT_URI || "http://localhost:8000/callback",
+        scope: 'identify guilds.join',
+        botToken: process.env.BOT_TOKEN
     },
     express: {
         port: process.env.PORT || 8000,
