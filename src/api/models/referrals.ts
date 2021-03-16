0cd40b11d53c444f4b5c378b4fc82fa71b23c2bb
Dominic Humber <dhumber2017@gmail.com>
2021-03-15 20:46:32 -0400

    Add more sql queries that I would need
    Update connection file to work with queries as well as executions

--- src/api/models/referrals.ts
+++ src/api/models/referrals.ts
@@ -2,46 +2,36 @@ import sql from '../utils/connection';
 
 export default class Referrals {
 
-    public static getInvite = async (id: string) => {
-        let result = await sql('SELECT COUNT(*) as count FROM invites WHERE id=CAST(? AS BIGINT)', [ id ]);
+    public static getInvite = async (id: string): Promise<[number, string]> => {
+        let result = await sql.query('SELECT COUNT(*) as count, userId FROM `invites` WHERE id=?', [ id ]);
 
-        console.log(result)
-
-        return result;
+        return [ result[0].count, result[0].userId ];
     }
 
     public static createInvite = async (userId: string, expiresIn?: string) => {
-        let result = await sql('INSERT INTO invites(id, userId, expiresIn) VALUES(UUID_SHORT(), ?, ?)', [ userId, expiresIn ]);
-
-        console.log(result);
+        await sql.execute('INSERT INTO invites(id, userId, expiresIn) VALUES(UUID_SHORT(), ?, ?)', [ userId, expiresIn ]);
     }
 
     public static addReferral = async (userId: string, inviterId: string) => {
-        let result = await sql('SELECT COUNT(*) FROM referrals WHERE userId = ?', [ userId ]);
+        let result = await sql.query('SELECT COUNT(*) as count FROM referrals WHERE userId = ?', [ userId ]);
 
-        console.log(result);
-
-        if (result) {
-            await sql('UPDATE referrals SET inviterId = ? WHERE userId = ?', [ inviterId, userId ])
+        if (result[0].count) {
+            await sql.execute('UPDATE referrals SET inviterId = ? WHERE userId = ?', [ inviterId, userId ])
         } else {
-            await sql('INSERT INTO referrals(inviterId, userId) VALUES(?, ?)', [ inviterId, userId ])
+            await sql.execute('INSERT INTO referrals(inviterId, userId) VALUES(?, ?)', [ inviterId, userId ])
         }
     }
 
     public static getReferrer = async (userId: string) => {
-        let result = await sql('SELECT inviterId, joinedAt FROM referrals WHERE userId = ?', [ userId ]);
+        let result = await sql.query('SELECT inviterId, joinedAt FROM referrals WHERE userId = ?', [ userId ]);
 
-        console.log(result);
-
-        return result;
+        return result[0];
     }
 
     public static getReferrals = async (inviterId: string) => {
-        let result = await sql('SELECT userId, joinedAt FROM referrals WHERE inviterId = ?', [ inviterId ]);
+        let results = await sql.query('SELECT userId, joinedAt FROM referrals WHERE inviterId = ?', [ inviterId ]);
 
-        console.log(result);
-
-        return result;
+        return results;
     }
 
 }
--- src/api/utils/connection.ts
+++ src/api/utils/connection.ts
@@ -1,9 +1,19 @@
-import mysql from 'mysql2/promise';
+import mysql, { RowDataPacket } from 'mysql2/promise';
 import config from './config';
 
-export default async (query: string, params?: Array<string | number | undefined>) => {
-    const connection = await mysql.createConnection(config.db);
-    const [ results ] = await connection.execute(query, params);
+export default {
+    execute: async (query: string, params?: Array<string | number | undefined>) => {
+        const connection = await mysql.createConnection(config.db);
+        const [ results ] = await connection.execute(query, params);
+        await connection.end();
 
-    return results;
+        return results;
+    },
+    query: async (query: string, params?: Array<string | number | undefined>) => {
+        const connection = await mysql.createConnection(config.db);
+        const [ results ] = await connection.query<RowDataPacket[]>(query, params);
+        await connection.end();
+
+        return results;
+    }
 }
